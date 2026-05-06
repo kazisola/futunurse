659a8fa832a0f1153ef52a668f28173c2e1c3a3a
Kazi Solayman <kazisolayman123@gmail.com>
2026-05-05 23:58:23 -0500

    Optimized NCLEX API endpoints

--- src/app/api/nclex/sessions/route.ts
+++ src/app/api/nclex/sessions/route.ts
@@ -5,6 +5,23 @@ import { NextRequest, NextResponse } from "next/server";
 import { authOptions } from "../../auth/[...nextauth]/route";
 import { RecentSession } from "@/models/NCLEX/RecentSessionModel";
 
+// GET RECENT NCLEX SESSIONS
+export async function GET(req: NextRequest) {
+    try {
+        await connectDB();
+        const session = await getServerSession(authOptions);
+        if (!session) {
+            return NextResponse.json({ success: false, message: "Unauthenticated!" }, { status: 401 });
+        }
+        const recentSessions = await RecentSession.find({ user: (session.user as { id: string }).id });
+        return NextResponse.json({ success: true, recentSessions }, { status: 200 });
+    } catch (error) {
+        console.log(error);
+        return handleApiError(error);
+    }
+}
+
+// SAVE NCLEX SESSION
 export async function POST(req: NextRequest) {
     try {
         await connectDB();
