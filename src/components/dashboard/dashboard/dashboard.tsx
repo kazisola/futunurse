db8f43197e8a527bce75226da036672be89d09f8
Kazi Solayman <kazisolayman123@gmail.com>
2026-05-18 18:40:37 -0500

    Added dashboard rtk query and consume the query from dashboard client

--- src/components/dashboard/dashboard/dashboard.tsx
+++ src/components/dashboard/dashboard/dashboard.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Calendar, Zap } from 'lucide-react';
+import { Calendar } from 'lucide-react';
 import React from 'react';
 import QuickInsights from './components/QuickInsights';
 import Features from './components/Features';
@@ -8,22 +8,20 @@ import WeekInsight from './components/WeekInsight';
 import AISuggestions from './components/AISuggestions';
 import StrengthsRadar from './components/StrengthsRadar';
 import AvgTimeChart from './components/AvgTimeChart';
-import { useGetDashboard } from '@/hooks/useGetDashboard';
 import DashboardSkeleton from './components/DashboardSkeleton';
+import { useGetDashboardQuery } from '@/redux/services/userApi';
 
 interface DashboardProps {
     user?: {
         name?: string | null;
-        email?: string | null;
         image?: string | null;
-        id?: string | null;
     }
 }
 
 export const Dashboard = ({ user }: DashboardProps) => {
     const today = new Date();
-    const { dashboardData, loading } = useGetDashboard();
-    if (loading || !dashboardData) return <DashboardSkeleton />;
+    const { data, isLoading: dashboardLoading } = useGetDashboardQuery()
+    if (dashboardLoading || !data?.dashboard) return <DashboardSkeleton />;
     return (
         <div className='space-y-5 max-sm:space-y-3'>
             <div className='mb-10 max-sm:mb-8 rounded-md w-full flex max-md:flex-col max-md:gap-3 md:items-center justify-between'>
@@ -33,19 +31,19 @@ export const Dashboard = ({ user }: DashboardProps) => {
                 </div>
                 {/* <div className='max-md:hidden text-white bg-linear-to-b bg-cyan-500 to-10% flex items-center gap-3 rounded-md text-xs px-3 py-2 font-medium'><Zap size={16} /> Pro Trial - 14 days remaining</div> */}
             </div>
-            <QuickInsights quickInsights={dashboardData} />
+            <QuickInsights carePlans={ data?.dashboard.carePlans} nclexInsights={data?.dashboard.nclexInsights} />
             <div className='md:grid grid-cols-3 gap-5 max-sm:space-y-5'>
                 <div className='col-span-2 space-y-5'>
                     <Features />
-                    <NclexPerformance nclexTrend={dashboardData?.nclexTrend} />
+                    <NclexPerformance nclexTrend={data?.dashboard.nclexTrend} />
                     <div className='grid md:grid-cols-2 gap-5'>
                         <AvgTimeChart />
-                        <StrengthsRadar strength={dashboardData?.performanceByCategory} />
+                        <StrengthsRadar strength={data?.dashboard.performanceByCategory} />
                     </div>
                 </div>
                 <div className='col-span-1 space-y-5'>
-                    <AISuggestions ai_suggestions={dashboardData?.suggestions} />
-                    <WeekInsight weekInsight={{ carePlans: dashboardData?.carePlans?.number_of_week_care_plans, completedQuestions: dashboardData?.nclexInsights?.week_completed_questions }} />
+                    <AISuggestions ai_suggestions={data?.dashboard.suggestions} />
+                    <WeekInsight weekInsight={{ carePlans: data?.dashboard.carePlans?.number_of_week_care_plans, completedQuestions: data?.dashboard.nclexInsights?.week_completed_questions }} />
                 </div>
             </div>
         </div>
--- src/redux/services/userApi.ts
+++ src/redux/services/userApi.ts
@@ -1,13 +1,47 @@
 import { IUser } from "@/types/User";
 import { baseApi } from "./baseApi";
 
+type AISuggestion = {
+    title: string;
+    score?: number;
+    description: string;
+    type: "weakness" | "strength" | "pattern";
+}
+
+type Dashboard = {
+    carePlans: {
+        number_of_care_plans: number;
+        number_of_week_care_plans: number;
+    };
+    nclexInsights: {
+        total_completed_questions: number;
+        week_completed_questions: number;
+        overall_score: number;
+    },
+    nclexTrend: {
+        date: string;
+        totalQuestions?: number;
+        correctAnswers?: number;
+        score: number;
+    },
+    performanceByCategory: {
+        category: string;
+        averageScore: number;
+    },
+    suggestions: AISuggestion[]
+};
+
 export const userApi = baseApi.injectEndpoints({
     endpoints: (builder) => ({
+        getDashboard: builder.query<{ dashboard: Dashboard}, void>({
+            query: () => '/dashboard',
+            providesTags: ['User']
+        }),
         getUser: builder.query<{ user: IUser }, void>({
             query: () => '/user',
             providesTags: ['User']
         }),
-        updateUser: builder.mutation<{ user: IUser}, { data: Partial<IUser> }>({
+        updateUser: builder.mutation<{ user: IUser }, { data: Partial<IUser> }>({
             query: ({ data }) => ({
                 url: '/user',
                 method: 'PATCH',
@@ -18,4 +52,4 @@ export const userApi = baseApi.injectEndpoints({
     })
 })
 
-export const { useGetUserQuery, useUpdateUserMutation } = userApi;
+export const { useGetDashboardQuery, useGetUserQuery, useUpdateUserMutation } = userApi;
