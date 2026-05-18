0e5f58be37fcb5f9996d1efc0121d0b961a3327f
Kazi Solayman <kazisolayman123@gmail.com>
2026-05-17 23:57:36 -0500

    Optimized care plans skeleton for loading state

--- src/components/dashboard/care-plans/CarePlans.tsx
+++ src/components/dashboard/care-plans/CarePlans.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 import CarePlan from './components/CarePlan';
 import { ICarePlan } from '@/types/PatientCarePlan';
-import { Skeleton } from '@/components/ui/skeleton';
+import CarePlansSkeleton from './components/CareplansSkeleton';
 
 export const CarePlans = () => {
     const [carePlans, setCarePlans] = useState<ICarePlan[]>([]);
@@ -32,32 +32,7 @@ export const CarePlans = () => {
         <div className='space-y-8'>
 
             {plansLoading ? (
-                <>
-                    <div className='flex items-start md:justify-between max-md:flex-col gap-8'>
-                        <div className='w-full'>
-                            <Skeleton className='w-72 h-10 mb-2' />
-                            <Skeleton className='w-120 h-5' />
-                            <div className='flex items-center gap-4 mt-4'>
-                                <Skeleton className='w-28 h-6' />
-                                <Skeleton className='w-28 h-6 ' />
-                            </div>
-                        </div>
-                        <div className='flex items-center gap-2'>
-                            <Skeleton className='w-40 h-11' />
-                            <Skeleton className='w-40 h-11 ' />
-                        </div>
-                    </div>
-                    <div className="grid md:grid-cols-1 lg:grid-cols-2 gap-6">
-                        {
-                            Array.from({ length: 6 }).map((_, index) => (
-                                <Skeleton
-                                    key={index}
-                                    className="h-44"
-                                />
-                            ))
-                        }
-                    </div>
-                </>
+                <CarePlansSkeleton />
             ) : carePlans.length > 0 ? (
                 <>
                     <div className='flex md:justify-between max-md:flex-col max-md:gap-8'>
