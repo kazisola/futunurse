c76495a8244a05f6bdf7d7a5818489b1e0851c84
Kazi Solayman <kazisolayman123@gmail.com>
2026-05-19 13:33:15 -0500

    Saved cards design modified

--- src/components/dashboard/companion/components/SavedCards.tsx
+++ src/components/dashboard/companion/components/SavedCards.tsx
@@ -1,6 +1,7 @@
+import { Button } from "@/components/ui/button";
 import { CompanionCard, CompanionType } from "@/types/companion";
 import axios from "axios";
-import { FlaskConical, FolderSearch, Pill, Stethoscope } from 'lucide-react';
+import { FlaskConical, FolderSearch, MoreVertical, Pill, Stethoscope, Trash } from 'lucide-react';
 import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 
 const SavedCards = ({ setCard }: { setCard: Dispatch<SetStateAction<CompanionCard | null>> }) => {
@@ -44,21 +45,25 @@ const SavedCards = ({ setCard }: { setCard: Dispatch<SetStateAction<CompanionCar
                 <ul className="flex flex-col gap-1 max-h-60 overflow-y-auto">
                     {cards.map((card, index) => <li key={index} onClick={() => handleGetSavedCard(card._id)} className="flex justify-between items-center hover:bg-gray-100 py-1.5 px-2 rounded-md cursor-pointer">
                         <div className="flex items-center gap-2">
-                            <h5 className={`w-1.5 h-1.5 rounded-full ${card.type === 'drug' ? 'bg-blue-700' : card.type === 'lab' ? 'bg-rose-700' : 'bg-violet-700'}`}></h5>
+                            {/* <h5 className={`w-1.5 h-1.5 rounded-full ${card.type === 'drug' ? 'bg-blue-700' : card.type === 'lab' ? 'bg-rose-700' : 'bg-violet-700'}`}></h5> */}
+                            <div>
+                                {card.type === 'drug' ?
+                                    <Pill className="w-3.5 h-3.5 text-blue-700" />
+                                    :
+                                    card.type === 'lab' ?
+                                        <FlaskConical className="w-3.5 h-3.5 text-rose-700" />
+                                        :
+                                        card.type === 'diagnostic' ?
+                                            <Stethoscope className="w-3.5 h-3.5 text-violet-700" />
+                                            :
+                                            null
+                                }
+                            </div>
                             <p className="text-sm capitalize">{card.query.length > 25 ? `${card.query.slice(0, 25)}...` : card.query}</p>
                         </div>
                         <div>
-                            {card.type === 'drug' ?
-                                <Pill className="w-3.5 h-3.5 text-slate-800" />
-                                :
-                                card.type === 'lab' ?
-                                    <FlaskConical className="w-3.5 h-3.5 text-slate-800" />
-                                    :
-                                    card.type === 'diagnostic' ?
-                                        <Stethoscope className="w-3.5 h-3.5 text-slate-800" />
-                                        :
-                                        null
-                            }
+                            <Button size={'sm'} className="bg-transparent hover:bg-gray-200/60 text-gray-800"><MoreVertical /></Button>
+                            {/* <Button size={'sm'} className="bg-transparent hover:bg-gray-200/60 text-red-400/80 hover:text-red-400"><Trash className="w-3.5 h-3.5" /></Button> */}
                         </div>
                     </li>)}
                 </ul>
