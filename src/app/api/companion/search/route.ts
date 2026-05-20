11682992c5cd3837cab2a41d1c56fb3aeb6d0523
Kazi Solayman <kazisolayman123@gmail.com>
2026-05-19 23:40:41 -0500

    Enhanced companion search api route for LLM calling to create companion card data by returning card if already exists based on query and type

--- src/app/api/companion/search/route.ts
+++ src/app/api/companion/search/route.ts
@@ -1,5 +1,5 @@
 import { connectDB } from "@/lib/mongoose";
-import { CompanionType } from "@/types/companion";
+import { CompanionCard, CompanionType } from "@/types/companion";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 import { authOptions } from "../../auth/[...nextauth]/route";
@@ -8,6 +8,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import { configDotenv } from "dotenv";
 import { extractJson } from "@/lib/extractJson";
 import { handleApiError } from "@/lib/apiError";
+import { CompanionSaved } from "@/models/Companion/CompanionSavedModel";
 configDotenv();
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string)
@@ -16,8 +17,8 @@ export async function GET(req: NextRequest) {
     try {
         const { searchParams } = new URL(req.url)
 
-        const query = searchParams.get("query")?.trim()
-        const type = searchParams.get("type") as CompanionType
+        const query = searchParams.get("query")?.toLowerCase().trim()
+        const type = searchParams.get("type")?.toLowerCase() as CompanionType
 
         if (!query || query.length < 2) {
             return NextResponse.json({
@@ -38,6 +39,21 @@ export async function GET(req: NextRequest) {
         if (!session) {
             return NextResponse.json({ success: false, message: "User unauthenticated!" }, { status: 401 });
         }
+
+        // Return generated card if already saved
+        const existing = await CompanionSaved.findOne({
+            user: (session.user as { id: string }).id,
+            query,
+            type
+        })
+            .select("card -_id")
+            .lean<{ card: CompanionCard }>();
+            
+        if (existing) {
+            return NextResponse.json({ success: true, card: existing.card })
+        }
+
+        console.log("------------------------------MOVING ON TO AI GENERATION------------------------------")
 
         const system_prompt_map = {
             drug: DRUG_SYSTEM_PROMPT,
@@ -65,7 +81,7 @@ export async function GET(req: NextRequest) {
         const raw_content = result.response.text();
 
         const card = extractJson(raw_content)
-
+        console.log("card from ai:", card)
         return NextResponse.json({ success: true, card })
     } catch (error) {
         console.log(error);
